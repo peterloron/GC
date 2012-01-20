878a9d44fdfc97c3d8817c8168a417b0ac953b48
Peter Loron <peterl@standingwave.org>
2012-01-19 22:42:59 -0800

    Cleaning stuff up.

--- src/main.py
+++ src/main.py
@@ -1,75 +1,101 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # encoding: utf-8
 """
-untitled.py
+Mightyohm Geiger Counter serial interface
 
-Created by Peter Loron on 2010-12-25.
-Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+Created by Peter Loron on 2011-10-06.
 """
-import serial
 import ConfigParser
 import logging
 import sys
+import serialReader
+import pachube
+import time
+import Queue
+import traceback
 
-logging.basicConfig(level=logging.INFO)
-
-port = ""
-speed = 0
-con = serial.Serial()
+CONFIG_FILE = "config.txt"
+config = None
+log_file = "console"
+log_level = logging.WARN
 
 
 def ReadConfiguration():
-    global port
-    global speed
-
+    global CONFIG_FILE
+    global persistance_modules
+    global log_level
+    global log_file
+    
     try:
         config = ConfigParser.SafeConfigParser()
-        config.read('config.txt')
-    
-        port = config.get("serial", "port")
-        speed = config.get("serial","speed")
-    except Exception, e:
-        logging.error("Could not read config: \n\n%s" % (e))
-        sys.exit(1)      
-
-
-def OpenSerialConnection():
-    global speed
-    global port
-    global con
-    
-    try:
-        con.port = port
-        con.speed = speed
-        con.timeout = 5
-        con.open()
-    except serial.SerialException, e:
-        logging.error("Could not open serial port %s: \n\n%s" % (con.portstr, e))
-        sys.exit(1)    
-
-
-def CloseSerialConnection():
-    global con
-    con.close()
-
-
-def ReadSerialData():
-    global con
-    
-    try:
-        # loop-a-doop!
-        while True:
-            s = con.readline()
-            print u"Got some!"
-            #f.write(s)
-            #f.flush()
-    except Exception, e:
-        logging.error("Failure while reading serial data.\n\n%s" % (e,))
+        config.read(CONFIG_FILE)
         
+        log_level = config.get("core","log_level")
+        log_file = config.get("core","log_file")
+
+    except Exception, e:
+        print("Could not read config: \n\n" + e)
+        sys.exit(1)
+    finally:
+        return config
+
+
 
 ########################################################
 
-ReadConfiguration()
-OpenSerialConnection()
-CloseSerialConnection()
+def main():
+    global log_level
+    global log_file
+    global config
+    
+    # load config from disk
+    config = ReadConfiguration()   
+    
+    # define and configure logging
+    LEVELS = { 'debug':logging.DEBUG,
+                'info':logging.INFO,
+                'warning':logging.WARNING,
+                'error':logging.ERROR,
+                'critical':logging.CRITICAL,
+                }
+    
+    ll = LEVELS.get(log_level, logging.NOTSET)
+    if log_file == 'console':
+        logging.basicConfig(level=ll, format="%(asctime)s -- %(message)s")
+    else:
+        logging.basicConfig(level=ll, format="%(asctime)s -- %(message)s", filename=log_file)
+
+
+
+    # this is there the raw data from the serial reader will live until processed
+    raw_data = Queue.Queue()
+
+    try:
+        # set up and start the serial reader thread
+        sr = serialReader.SerialReader(config,raw_data)
+        sr.setDaemon(True)
+        sr.start()
+        
+        # set up and start the data processor thread
+        pc = pachube.Pachube(config,raw_data)
+        pc.setDaemon(True)
+        pc.start()
+        
+        
+        # now that we're running, just wait for an interrupt...
+        logging.info("Threads initialized. Running...")
+        while(True):
+            time.sleep(1)
+        
+    except KeyboardInterrupt:
+        logging.debug("Keyboard interrupt caught. Stopping threads.")
+        sr.stop_thread()
+        pc.stop_thread()
+    finally:
+        logging.info("Done.")
+        pass
+
+    
+if __name__ == '__main__':
+    main()
 
